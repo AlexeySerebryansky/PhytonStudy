799ce0789d888fa010d039c81d2d92357fd0f778
Krays <git@github.com:AlexeySerebryansky/StudyPhyton.git>
2025-07-08 20:56:06 +0300

    commit

---
+++ lessons/lesson2/lesson2.py
@@ -0,0 +1,23 @@
+#year_of_birth = 1995
+#print(id(year_of_birth))
+import math
+import random
+
+#new_year_of_birth = "year_of_birth +1"
+#print(id(year_of_birth))
+
+#ome = 12321
+#rint(id(some))
+
+number_one = 12
+number_two = int(input('Enter value : '))
+
+print(number_two ** number_one)
+print(number_two // number_one)
+print(number_two % number_one)
+
+print(divmod(number_one, number_two))
+
+print(math.sqrt(number_two))
+
+
