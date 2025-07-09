25103883a84d6f2a58d6cdb60cbe41ad5f877b9a
Krays <git@github.com:AlexeySerebryansky/StudyPhyton.git>
2025-07-09 23:21:48 +0300

    commit

--- home_works/lesson2/output_to_column.py
+++ home_works/lesson2/output_to_column.py
@@ -1,1 +1,16 @@
-number = input('Input your numbers : ')
+number = int(input('Enter your 4 digit number : '))
+
+CONST1000 = 1000
+CONST100 = 100
+CONST10 = 10
+
+number1 = number//CONST1000
+number2 = (number % CONST1000 - number % CONST100) // CONST100
+number3 = (number % CONST100 - number % CONST10) // CONST10
+number4 = number % CONST10
+
+print(number1)
+print(number2)
+print(number3)
+print(number4)
+
