fd7f059020c2e94e282679dc692779c2d96d7349
Krays <git@github.com:AlexeySerebryansky/StudyPhyton.git>
2025-07-10 22:18:41 +0300

    commit

---
+++ home_works/lesson2/revers_numbers.py
@@ -0,0 +1,20 @@
+
+
+CONST10000 = 10000
+CONST1000 = 1000
+CONST100 = 100
+CONST10 = 10
+
+print("Enter your 5 values number : ")
+
+number = int(input())
+
+number1 = number // CONST10000
+number2 = (number // CONST1000) % CONST10
+number3 = (number % CONST1000) // CONST100
+number4 = (number % CONST100) // CONST10
+number5 = (number % CONST10)
+
+print("Your number is : ", number)
+print("But its opposite number is : ")
+print(number5, number4, number3, number2, number1)
