2b92285e0f9b7d6bdfe7b31fb5fa7581c4ef2837
Krays <git@github.com:AlexeySerebryansky/StudyPhyton.git>
2025-07-19 19:04:18 +0300

    commit

---
+++ home_works/lesson4/home_work_4.3.py
@@ -0,0 +1,14 @@
+import random
+
+lst_len = random.randint(3, 10)
+print(lst_len)
+
+lst = []
+
+for i in range(lst_len):
+    number = int(random.random() * 10)
+    lst.append(number)
+
+print(lst)
+lst2 = [lst[0], lst[1], lst[-1]]
+print(lst2)
