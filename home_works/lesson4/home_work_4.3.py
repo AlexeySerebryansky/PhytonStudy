348f8ab0f48cb5b25e93d3ec88d333e602540d32
Krays <git@github.com:AlexeySerebryansky/StudyPhyton.git>
2025-07-19 19:31:21 +0300

    commit

--- home_works/lesson4/home_work_4.3.py
+++ home_works/lesson4/home_work_4.3.py
@@ -8,7 +8,7 @@ lst = []
 for i in range(lst_len):
     number = int(random.random() * 10)
     lst.append(number)
-
 print(lst)
+
 lst2 = [lst[0], lst[1], lst[-1]]
 print(lst2)
---
+++ lessons/lesson5/lesson5.py
@@ -0,0 +1,9 @@
+first_string = "Hello world"
+second_string = str(123)
+
+# print(first_string[0])
+# print(first_string + second_string)
+# print(first_string * 10)
+
+for x in first_string:
+    print(x)
