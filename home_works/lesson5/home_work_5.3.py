641688f29a8cfcd0e50d936e697689cb8cc04ade
Krays <git@github.com:AlexeySerebryansky/StudyPhyton.git>
2025-07-25 21:55:39 +0300

    commit

--- home_works/lesson5/home_work_5.3.py
+++ home_works/lesson5/home_work_5.3.py
@@ -1,18 +1,14 @@
-import re
+import string
+
 print("Enter your text :")
 input_string = str(input())
 
-# input_string = re.sub('[?!@#$.,]', '', input_string)
-
-input_string = input_string.replace(",", "")
-input_string = input_string.replace(".", "")
-input_string = input_string.replace("?", "")
-input_string = input_string.replace("!", "")
+input_string = ''.join(char for char in input_string if char not in string.punctuation)
 
 input_string = input_string.title()
 input_string = input_string.replace(" ", "")
-
 input_string = input_string[:140]
+
 print("#" + input_string)
 
 
---
+++ lessons/lesson6/lesson6.py
@@ -0,0 +1,44 @@
+# tup = (1, 2, 3, "4", [5, "6"])
+# print(tup)
+#
+# tup1, tup2 = (1, (2, 3))
+# print(tup1)
+# print(tup2)
+
+
+# for index, elem in enumerate([1, 2, 3]):
+#     print(index, '->', elem)
+
+# print(tup[1:3])
+
+# new_dict = {
+#     "name" : "Alex",
+#     "age" : 26,
+#     "group" : {
+#         "name" : "PM-14-1",
+#         "count_of_student" : 21,
+#         "starosta_name" : "Kolya"
+#     }
+# }
+
+# print(new_dict.keys())
+# print(new_dict.values())
+
+# for key, value in new_dict.items():
+#     print(key, '->', value)
+
+# print(new_dict.pop("group"))
+# print(new_dict)
+
+# print(new_dict["name"])
+# new_dict["name"] = "Katya"
+# print(new_dict)
+
+# new_dict["avaregw_mark"] = 79
+# print(new_dict)
+
+
+# new_dict = dict(name = "Katya", age = 25)
+#
+# print("name" in new_dict)
+# print("Katya" in new_dict)
