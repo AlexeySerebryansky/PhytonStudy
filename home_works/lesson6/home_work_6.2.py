a236f666e9b22b968fa74545730add7410d81c45
Krays <git@github.com:AlexeySerebryansky/StudyPhyton.git>
2025-07-27 18:05:33 +0300

    commit

--- home_works/lesson6/home_work_6.2.py
+++ home_works/lesson6/home_work_6.2.py
@@ -1,16 +1,17 @@
 number = 224930
 
-time = {
-    "day" : 86400,
-    "hour" : 3600,
-    "minutes" : 60
-}
+DAY = 86400
+HOUR = 3600
+MINUTES = 60
+
+days, suspension_second = divmod(number, DAY)
+hours, suspension_second = divmod(suspension_second, HOUR)
+minutes, seconds = divmod(suspension_second, MINUTES)
+
+hours_str = str(hours).zfill(2)
+minutes_str = str(minutes).zfill(2)
+seconds_str = str(seconds).zfill(2)
+
+print(days, "days,", f"{hours_str}:{minutes_str}:{seconds_str}")
 
 
-day = number//time["day"]
-hour = (number - (time["day"] * 2)) // time["hour"]
-minutes = ((number - (number//time["day"])) - (number - (time["day"] * 2)) // time["hour"]) // time["minutes"]
-
-
-
-print(day, "days, ", hour, ":", minutes )
