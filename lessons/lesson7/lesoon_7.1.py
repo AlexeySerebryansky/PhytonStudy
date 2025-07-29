879e6ae704e4abf5cd4e8da8aa6c3ad9b59fd93f
Krays <git@github.com:AlexeySerebryansky/StudyPhyton.git>
2025-07-29 21:39:21 +0300

    commit

---
+++ lessons/lesson7/lesoon_7.1.py
@@ -0,0 +1,7 @@
+def add_numbers(first_number: int, second_number: int) -> int:
+    return first_number + second_number
+
+
+result = add_numbers(1, 3)
+
+print(2 + result)
---
+++ lessons/lesson7/lesson7.py
@@ -0,0 +1,26 @@
+# myset = {1, 2, 3, 4, 1, 3}
+#
+# myset.add(5)
+# print(myset)
+#
+# myset.remove(2)
+# print(myset)
+#
+# myset.discard(22)
+# print(myset)
+
+
+# first_set = {1, 2, 3}
+# second_sed = {3, 4, 5}
+#
+# print(first_set.union(second_sed))
+# print(first_set.intersection(second_sed))
+# print(first_set.difference(second_sed))
+#
+# frozenset_set = frozenset([1, 2, 3, 5, 1, 2])
+#
+# print(frozenset_set)
+#
+# print(first_set.intersection(frozenset_set))
+
+
