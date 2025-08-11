d661898f8cbec8d8227b5f719577ab786c9209e9
Krays <git@github.com:AlexeySerebryansky/StudyPhyton.git>
2025-08-11 15:31:34 +0300

    commit

---
+++ home_works/lesson7/home_work_7.2.py
@@ -0,0 +1,16 @@
+def correct_sentence(text: str) -> str:
+
+    sentence = text[0].upper()+text[1:]
+
+    if sentence[-1] == ".":
+        return sentence
+    else:
+        return sentence.rstrip("") + "."
+
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
