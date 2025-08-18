3d78cdf440500437e57e0d330bd729ccf39a82f4
Krays <git@github.com:AlexeySerebryansky/StudyPhyton.git>
2025-08-18 10:57:56 +0300

    commit

--- home_works/lesson_10/home_work_10.2.py
+++ home_works/lesson_10/home_work_10.2.py
@@ -1,24 +1,21 @@
-import string
+def first_word(text: str) -> str:
+    word = ""
+    in_word = False
+
+    for char in text:
+        if char.isalpha() or char == "'":
+            word += char
+            in_word = True
+        elif in_word:
+            break
+
+    return word
 
 
-def first_word(text):
-    lst_words = [(lambda x: x)(x) for x in range(len(text - 1))]
-    print(lst_words)
-
-    pass
-
-
-first_word("Hello world")
-
-
-
-
-
-
-# assert first_word("Hello world") == "Hello", 'Test1'
-# assert first_word("greetings, friends") == "greetings", 'Test2'
-# assert first_word("don't touch it") == "don't", 'Test3'
-# assert first_word(".., and so on ...") == "and", 'Test4'
-# assert first_word("hi") == "hi", 'Test5'
-# assert first_word("Hello.World") == "Hello", 'Test6'
-# print('OK')
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
